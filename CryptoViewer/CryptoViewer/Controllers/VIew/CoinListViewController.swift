11817f212978bf55f8b76d3ee9780ffe857e03eb
Arian Mohajer <amohajer45@gmail.com>
2022-02-09 18:41:19 -0700

    fixed filtering. minor UI changes

--- CryptoViewer/CryptoViewer/Controllers/VIew/CoinListViewController.swift
+++ CryptoViewer/CryptoViewer/Controllers/VIew/CoinListViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class CoinListViewController: UIViewController {
-
+    
     @IBOutlet weak var searchBar: UISearchBar!
     
     var topLevelDictionary: TopLevelDictionary? {
         didSet {
             DispatchQueue.main.async {
+                self.searchBar.showsCancelButton = true
+                self.searchBar.isHidden = false
                 self.coinListTableView.reloadData()
             }
         }
@@ -25,7 +27,7 @@ class CoinListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         coinListTableView.delegate = self
         coinListTableView.dataSource = self
         searchBar.delegate = self
@@ -40,9 +42,15 @@ class CoinListViewController: UIViewController {
             switch result {
                 
             case .success(let tld):
+                
+                DispatchQueue.main.async {
+                    self.searchBar.isHidden = true
+                }
+                
                 self.topLevelDictionary = tld
                 
                 DispatchQueue.main.async {
+                    
                     self.coinListTableView.reloadData()
                 }
             case .failure(let error):
@@ -52,33 +60,63 @@ class CoinListViewController: UIViewController {
     }
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if segue.identifier == "toCoinDetails" {
             if let index = coinListTableView.indexPathForSelectedRow {
                 if let destination = segue.destination as? CoinDetailsViewController {
                     
-                    guard let label = topLevelDictionary?.coins[index.row].label else { return }
-                    
-                    var ticker = ""
-                    
-                    for labelChar in label {
-                        if labelChar == "/" {
-                            break
+                    if let searchText = searchBar.searchTextField.text,
+                       !searchText.isEmpty,
+                       let filteredDictionary = filteredTopLevelDictionary {
+                        
+                        let label = filteredDictionary.coins[index.row].label
+                        
+                        var ticker = ""
+                        
+                        for labelChar in label {
+                            if labelChar == "/" {
+                                break
+                            }
+                            ticker.append(labelChar)
                         }
-                        ticker.append(labelChar)
+                        
+                        NetworkController.fetchConversion(firstCurrencyTicker: ticker.lowercased(), secondCurrencyTicker: "btc") { result in
+                            switch result {
+                            case .success(let conversion):
+                                destination.ticker = ticker
+                                destination.conversion = conversion
+                            case .failure(let error):
+                                print(error)
+                            }
+                        }
+
+                    } else {
+                        
+                        guard let label = topLevelDictionary?.coins[index.row].label else { return }
+                        
+                        var ticker = ""
+                        
+                        for labelChar in label {
+                            if labelChar == "/" {
+                                break
+                            }
+                            ticker.append(labelChar)
+                        }
+                        
+                        NetworkController.fetchConversion(firstCurrencyTicker: ticker.lowercased(), secondCurrencyTicker: "btc") { result in
+                            switch result {
+                            case .success(let conversion):
+                                destination.ticker = ticker
+                                destination.conversion = conversion
+                            case .failure(let error):
+                                print(error)
+                            }
+                        }
                     }
                     
-                    NetworkController.fetchConversion(firstCurrencyTicker: ticker.lowercased(), secondCurrencyTicker: "btc") { result in
-                        switch result {
-                        case .success(let conversion):
-                            destination.ticker = ticker
-                            destination.conversion = conversion
-                        case .failure(let error):
-                            print(error)
-                        }
-                    }
                 }
             }
         }
@@ -87,18 +125,34 @@ class CoinListViewController: UIViewController {
 
 extension CoinListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let topLevelDictionary = topLevelDictionary else { return 0 }
-        return topLevelDictionary.coins.count
+        if searchBar.searchTextField.text == "" {
+            guard let topLevelDictionary = topLevelDictionary else { return 0 }
+            return topLevelDictionary.coins.count
+        } else {
+            guard let filteredTopLevelDictionary = filteredTopLevelDictionary else { return 0 }
+            return filteredTopLevelDictionary.coins.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "coinCell", for: indexPath) as? CoinTableViewCell else { return UITableViewCell() }
         
-        guard let topLevelDictionary = topLevelDictionary else { return cell }
+        if let searchText = searchBar.searchTextField.text,
+           !searchText.isEmpty {
+            guard let filteredTopLevelDictionary = filteredTopLevelDictionary else { return cell }
+            
+            cell.coin = filteredTopLevelDictionary.coins[indexPath.row]
+            cell.updateViews()
+            cell.selectionStyle = .none
+            
+        } else {
+            guard let topLevelDictionary = topLevelDictionary else { return cell }
+            
+            cell.coin = topLevelDictionary.coins[indexPath.row]
+            cell.updateViews()
+            cell.selectionStyle = .none
+        }
         
-        cell.coin = topLevelDictionary.coins[indexPath.row]
-        cell.updateViews()
-        cell.selectionStyle = .none
         
         return cell
     }
@@ -106,28 +160,35 @@ extension CoinListViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension CoinListViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
         if searchText == "" {
-            getFullCoinList()
+            DispatchQueue.main.async {
+                self.coinListTableView.reloadData()
+            }
         } else {
             updateResults(with: searchText)
         }
     }
     
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.searchTextField.text = ""
+        searchBar.resignFirstResponder()
+        getFullCoinList()
+    }
+    
     func updateResults(with searchText: String) {
-        
-        
         guard let topLevelDictionary = topLevelDictionary else { return }
         
         var filteredCoinsArray: [Coin] = []
         
         for coin in topLevelDictionary.coins {
-            if coin.name.hasPrefix(searchText) {
+            if coin.name.lowercased().contains(searchText.lowercased()) {
                 filteredCoinsArray.append(coin)
             }
         }
         
         filteredTopLevelDictionary = TopLevelDictionary(coins: filteredCoinsArray)
-        self.topLevelDictionary = filteredTopLevelDictionary
+        //self.topLevelDictionary = filteredTopLevelDictionary
         
         DispatchQueue.main.async {
             self.coinListTableView.reloadData()
